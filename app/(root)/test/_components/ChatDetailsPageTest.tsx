baa38c4b4cb11d08ce04268eee1c54842aa7a10f
Watch <dwatchr.man@gmail.com>
2024-03-11 23:11:13 +0600

    testing components

---
+++ app/(root)/test/_components/ChatDetailsPageTest.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface pageProps {
+    testId: string;
+}
+const ChatDetailsPageTest = ({ testId }: pageProps) => {
+    return <div>ChatDetailsPageTest</div>;
+};
+
+export default ChatDetailsPageTest;
---
+++ app/(root)/test/_components/ChatListPageTest.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+interface pageProps {
+    testId: string;
+}
+const ChatListPageTest = ({ testId }: pageProps) => {
+    return <div>ChatListPageTest</div>;
+};
+
+export default ChatListPageTest;
