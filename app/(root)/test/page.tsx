2b7f830029fc9c24eab66a33a54d7b6daa15e2ef
Watch <dwatchr.man@gmail.com>
2024-03-16 00:49:07 +0600

    added: redis

--- app/(root)/test/page.tsx
+++ app/(root)/test/page.tsx
@@ -5,18 +5,18 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+import { database } from "@/lib/db";
 // import AddFriend from "../chats/_components/AddFriend";
 
-const TestingComponents = () => {
+const TestingComponents = async () => {
+    // await database.set("hello", "world");
     return (
         <>
             <Popover>
                 <PopoverTrigger className='h-10 px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90 rounded-md'>
                     open
                 </PopoverTrigger>
-                <PopoverContent>
-                    {/* <AddFriend /> */}
-                </PopoverContent>
+                <PopoverContent>{/* <AddFriend /> */}</PopoverContent>
             </Popover>
         </>
     );
